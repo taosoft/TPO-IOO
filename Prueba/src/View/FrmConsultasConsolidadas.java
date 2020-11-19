e5030443def678e67dfc1a28d4a03c5fede4f651
shark sistemas srl <luciano.arias@sharksistemas.com.ar>
2020-11-18 21:21:13 -0300

    filtro de fechas

--- Prueba/src/View/FrmConsultasConsolidadas.java
+++ Prueba/src/View/FrmConsultasConsolidadas.java
@@ -69,7 +69,7 @@ public class FrmConsultasConsolidadas extends JDialog {
 
         var socio = mdlSocio.CrearSocio("Mario","30715645579","Empresa S.A.",tipoEmpresa.Mediana,
                 "comercialización", "libertadores 123","353535","dasd@sadas.com",
-                new Date(), tipoSocio.Participe);
+                new Date("13/10/2014"), tipoSocio.Participe);
 
         this.ctrSocio.AddSocio((socio));
 
@@ -77,7 +77,7 @@ public class FrmConsultasConsolidadas extends JDialog {
 
         socio = mdlSocio.CrearSocio("Juan","30801032158","Luz S.A.",tipoEmpresa.Mediana,
                 "comercialización", "Chacabuco 123","353535","dasd@sadas.com",
-                new Date(), tipoSocio.Participe);
+                new Date("26/09/2016"), tipoSocio.Participe);
 
         this.ctrSocio.AddSocio((socio));
 
@@ -85,7 +85,7 @@ public class FrmConsultasConsolidadas extends JDialog {
 
         socio = mdlSocio.CrearSocio("Martha","30715248547","La Risa SRL.",tipoEmpresa.Grande,
                 "Cotillon", "Rivadavia 4123","45484542","lalal@sadas.com",
-                new Date(), tipoSocio.Protector);
+                new Date("03/04/2008"), tipoSocio.Protector);
 
         this.ctrSocio.AddSocio((socio));
 
@@ -93,7 +93,7 @@ public class FrmConsultasConsolidadas extends JDialog {
 
         socio = mdlSocio.CrearSocio("Ledesma","27542547852","Gandoriza SA",tipoEmpresa.Pequena,
                 "Turismo", "Larralde 4251","151254215","dasd@sadas.com",
-                new Date(), tipoSocio.Participe);
+                new Date("12/10/2012"), tipoSocio.Participe);
 
         this.ctrSocio.AddSocio((socio));
     }
